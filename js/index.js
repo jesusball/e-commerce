f9f95e73fe02f6fcb4b97bd26b2ed8f538e65e70
Emirhan Karataş <emirhankaratasss34@gmail.com>
2022-12-25 01:21:38 +0300

    js/ sidebar click outside

--- js/index.js
+++ js/index.js
@@ -4,12 +4,20 @@ const btnOpenSidebar = document.querySelector("#btn-menu");
 const sidebar = document.querySelector("#sidebar");
 const btnCloseSidebar = document.querySelector("#close-sidebar");
 
-btnOpenSidebar.addEventListener("click", function(){
+btnOpenSidebar.addEventListener("click", function() {
     sidebar.style.left = "0";
-})
+});
 
-btnCloseSidebar.addEventListener("click", function(){
+btnCloseSidebar.addEventListener("click", function() {
     sidebar.style.left = "-100%";
-})
+});
+
+/* click outside start */
+document.addEventListener("click", function(event) {
+    if(!event.composedPath().includes(sidebar) && !event.composedPath().includes(btnOpenSidebar)){
+        sidebar.style.left = "-100%";
+    }
+});
+/* click outside end */
 
 //! home sidebar end
