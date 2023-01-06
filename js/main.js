310bff08c9c20d20a65a58feea5722895e9efece
Emirhan Karataş <emirhankaratasss34@gmail.com>
2023-01-06 22:05:17 +0300

    js/ product list düzenleme

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 import headerFunc from "./header.js";
 import productsFunc from "./products.js";
 
-//! add product to localStorage
-async function getData() {
+//! add product to localStorage start
+(async function() {
     const photos = await fetch("../js/data.json");
     const data = await photos.json();
 
     data ? localStorage.setItem("products", JSON.stringify(data)) : [];
-}
-
-getData();
-
-const products = localStorage.getItem("products");
+    productsFunc();
+})();
+//! add product to localStorage end
 
 
-
+//! add cartItems to localStorage start
 const cartItems = document.querySelector(".header-cart-count");
 
 cartItems.innerHTML = localStorage.getItem("cart") 
     ? JSON.parse(localStorage.getItem("cart")).length
     : "0";
+//! add cartItems to localStorage end
--- js/products.js
+++ js/products.js
@@ -88,4 +88,4 @@ function productsFunc() {
 }
 
 
-export default productsFunc();
+export default productsFunc;
